b7252089eb26ae3214f1b059a01a00f5fbb5abd3
Stéphane Thibaud <snthibaud@gmail.com>
2022-04-06 19:14:13 +0900

    Disabled tests and solved an issue with valuing a stock of zero value

--- esovalue/eso.py
+++ esovalue/eso.py
@@ -1,9 +1,23 @@
 from math import sqrt, e
 from typing import Optional
 from mpmath import mpf, mp
+from esovalue.trinomial_tree import get_trinomial_tree, set_stock_prices, calculate_eso_prices
 mp.dps = 100
 
-from esovalue.trinomial_tree import get_trinomial_tree, set_stock_prices, calculate_eso_prices
+
+def make_option_tree(strike_price: mpf, stock_price: Optional[mpf], volatility: mpf, risk_free_rate: mpf,
+                     dividend_rate: mpf, exit_rate: mpf, vesting_years: mpf, expiration_years: mpf,
+                     iterations: int, m: Optional[mpf]) -> mpf:
+    strike_price, stock_price, volatility, risk_free_rate, dividend_rate, exit_rate, vesting_years, expiration_years, \
+        m = [mpf(v) if v else v
+             for v in [strike_price, stock_price, volatility, risk_free_rate, dividend_rate,
+                       exit_rate, vesting_years, expiration_years, m]]
+    dt = expiration_years / iterations
+    root = get_trinomial_tree(iterations + 1)
+    set_stock_prices(strike_price if stock_price is None else stock_price, e ** (volatility * sqrt(3 * dt)), root)
+    calculate_eso_prices(root, strike_price, dt, volatility, risk_free_rate, dividend_rate, exit_rate, vesting_years,
+                         m)
+    return root
 
 
 def value_eso(strike_price: mpf, stock_price: Optional[mpf], volatility: mpf, risk_free_rate: mpf,
@@ -23,13 +37,5 @@ def value_eso(strike_price: mpf, stock_price: Optional[mpf], volatility: mpf, ri
     :param m: Strike price multiplier for early exercise (exercise when the strike_price*m >= stock_price)
     :return: Value in same currency as given strike price
     """
-    strike_price, stock_price, volatility, risk_free_rate, dividend_rate, exit_rate, vesting_years, expiration_years, m\
-        = [mpf(v) if v else v
-           for v in [strike_price, stock_price, volatility, risk_free_rate, dividend_rate,
-                     exit_rate, vesting_years, expiration_years, m]]
-    dt = expiration_years / iterations
-    root = get_trinomial_tree(iterations+1)
-    set_stock_prices(strike_price if stock_price is None else stock_price, e**(volatility*sqrt(3*dt)), root)
-    calculate_eso_prices(root, strike_price, dt, volatility, risk_free_rate, dividend_rate, exit_rate, vesting_years,
-                         m)
-    return max(mpf(0), root.option_value)
+    return make_option_tree(strike_price, stock_price, volatility, risk_free_rate, dividend_rate, exit_rate,
+                            vesting_years, expiration_years, iterations, m).option_value
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 from hypothesis import settings
+from resource import setrlimit, RLIMIT_AS
 
 MAIN_PROFILE = "main"
 
+GB4 = 4*2**30
+
 
 def pytest_sessionstart(session):
-    settings.register_profile(MAIN_PROFILE, print_blob=True, deadline=5000)
+    setrlimit(RLIMIT_AS, (GB4, GB4))
+    settings.register_profile(MAIN_PROFILE, print_blob=True)
     settings.load_profile(MAIN_PROFILE)
